79b908cc4aafea29276fbd2e054d7101cabff9a4
TaerAttO <surapun.khongthong@gmail.com>
2021-11-20 13:56:57 +0700

    Update fxmanifest.lua

--- lua/fxmanifest.lua
+++ lua/fxmanifest.lua
@@ -3,14 +3,17 @@ game 'common'
 
 name 'Monster MySQL'
 description 'Database wrapper for FiveM'
-version '1.8.0'
-url 'https://github.com/MonsterTaerAttO/monster_mysql'
 author 'TaerAttO'
+url 'https://github.com/MonsterTaerAttO/monster_mysql'
+website 'https://www.taeratto.dev'
+discord 'https://discord.gg/taeratto'
+version '0.0.1'
+server_only 'yes'
 use_fxv2_oal 'yes'
 lua54 'yes'
 
 server_scripts {
-	'monster_taeratto_mysql.js',
+	'*.monster_taeratto_mysql.js',
 	'wrapper.lua'
 }
 
